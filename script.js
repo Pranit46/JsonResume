e9aafe490ebf90119160d9d4a08c5f54bc50f2b2
Pranit Yadav <42487480+Pranit46@users.noreply.github.com>
2021-11-27 15:27:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let myResume = [
+  {
+    "PersonalInfo": {
+      "name": "Pranit Sanjay Yadav",
+      "email": "pranityadav@gmail.com",
+      "phone": "8879486224",
+      "degree": "BE (Information Technology)",
+      "github": "https://github.com/Pranit46",
+      "location": {
+        "address": "Nandadeep soc. Pltot no.77/c-2, Sawarkar Nagar",
+        "postalCode": "400606",
+        "city": "Thane",
+        "state": "Maharashatra",
+      }
+    }
+  },
+
+  {
+    "work": 
+      {
+        "company": "Course5i",
+        "position": "Survey Programmer",
+        "website": "https://www.course5i.com/",
+        "startDate": "04/06/2019",
+        "endDate":
+          "13/11/2021",
+        "summary":
+          "To program a link through which user can able to give the feedback for products made by client. With the help of that link we are collecting data which used for the further reseach purpose.",
+      }
+  },
+
+  {
+    "education": 
+      {
+        "institution": "Bharati Vidyapeeth college of engineering kharghar",
+        "area": "Kharghar",
+        "startYear": "2015",
+        "endYear": "2019",
+      }
+    },
+
+    {
+        "skills": 
+            {
+                "name": "HTML, CSS, Jquery ",
+                "level": "7",
+            }
+    },
+    {
+    "languages": 
+      {
+        "language": "English, Marathi, Hindi",
+        "fluency": "Marathi",
+      }
+    },
+    {
+    "interests": 
+      {
+        "name": "sports, trekking",
+        "keywords": [
+          "cricket, BGMI"
+        ],
+      }
+  },
+];
+
